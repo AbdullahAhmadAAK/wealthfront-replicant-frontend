4dd45d9243ba2f7f41f51d03f03129fe8d5704a9
AbdullahAhmadAAK <abdullahaak06@gmail.com>
2024-07-21 03:59:13 +0500

    Refactor code a bit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ const App = () => {
   }, [])
 
   useEffect(() => {
-    // Fetch data from the Express server
     axios.get(`${process.env.REACT_APP_API_BASE_URL}/stocks`)
       .then(response => {
         setStocksData(response.data)
@@ -37,16 +36,14 @@ const App = () => {
 
   return (
     <div className='bg-light-blue-to-light-orange pt-200px'>
-
       <div className='data-section'>
+        
         <div className='left-section'>
-          
           <div className="slider-container">
             <div id='risk-score-line'>
               <span id="risk-score">Risk Score: {selectedRiskScore.toFixed(1)}</span>
               <span id='portfolio-name'>Example Portfolio</span>
             </div>
-            
             <input
               type="range"
               id="risk-slider"
@@ -54,20 +51,18 @@ const App = () => {
               max="10"
               value={selectedRiskScore}
               onChange={(e) => handleChangeRiskScore(e)}
-            />
-            
+            />    
           </div>
 
           <div className='nonzero-stocks-section'>
             {selectedRiskStockData !== undefined && (
-
               Object.keys(selectedRiskStockData).map(key => {
-
                 if (key === "riskScore" || key === "_id") return;
 
                 const value = selectedRiskStockData[key]
                 if (value === 0.0) return;
 
+                // This can be refactored so that we find the matching mapping only once!
                 const title = stockKeyNameMappings.find(mapping => mapping.key === key)?.name
                 const bgColor = stockKeyNameMappings.find(mapping => mapping.key === key)?.bgColor
 
@@ -81,15 +76,12 @@ const App = () => {
                   </div>
                 )
               })
-
             )}
           </div>
 
           <div className='zero-stocks-section'>
             {selectedRiskStockData !== undefined && (
-
               Object.keys(selectedRiskStockData).map(key => {
-
                 if (key === "riskScore" || key === "_id") return;
 
                 const value = selectedRiskStockData[key]
@@ -104,25 +96,19 @@ const App = () => {
                   </div>
                 )
               })
-
             )}
           </div>
-
-
         </div>
 
         <div className='right-section'>
-
           <div className='right-section-info'>
             <h4 className='text-40 margin-0 purple-text'>Smarter investing, brilliantly personalized.</h4>
             <p className='purple-text'>Just answer a few questions, and we’ll build you a personalized portfolio of low-cost index funds from up to 17 global asset classes. Our software handles all the trading, rebalancing, and other busywork to help grow your wealth for the long term.</p>
             <button className='get-started-btn'>Get Started</button>
-          </div>
-          
+          </div>  
         </div>
 
       </div>
-
     </div>
   );
 };
