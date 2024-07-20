f1b0dfc265e4bd31d47a8b2be40d2a17781dbc6e
AbdullahAhmadAAK <abdullahaak06@gmail.com>
2024-07-21 02:28:42 +0500

    Majority of UI work

--- src/App.js
+++ src/App.js
@@ -32,37 +32,96 @@ const App = () => {
 
   const handleChangeRiskScore = (e) => {
     const newRiskScore = parseFloat(e.target.value)
-    console.log('New risk score: ', newRiskScore, typeof(newRiskScore))
     setSelectedRiskScore(newRiskScore)
   }
 
   return (
-    <div>
-      <h1>Stocks App</h1>
+    <div className='bg-light-blue-to-light-orange pt-200px'>
 
-      <button onClick={() => console.log(selectedRiskStockData, selectedRiskScore, stocksData)}>test me bro</button>
+      <div className='data-section'>
+        <div className='left-section'>
+          
+          <div className="slider-container">
+            <div style={{ display: 'flex', justifyContent: "space-between"}}>
+              <span id="risk-score">Risk Score: {selectedRiskScore.toFixed(1)}</span>
+              <span>Example Portfolio</span>
+            </div>
+            
+            <input
+              type="range"
+              id="risk-slider"
+              min="0"
+              max="10"
+              value={selectedRiskScore}
+              onChange={(e) => handleChangeRiskScore(e)}
+            />
+            
+          </div>
 
-      <label htmlFor="riskScore">Risk Score</label>
-      <select id='riskScore' value={selectedRiskScore} onChange={(e) => handleChangeRiskScore(e)}>
-        <option value={0}>0</option>
-        <option value={1}>1</option>
-        <option value={2}>2</option>
-      </select>      
+          <div className='nonzero-stocks-section'>
+            {selectedRiskStockData !== undefined && (
 
-      {selectedRiskStockData !== undefined && Object.keys(selectedRiskStockData).map(key => {
+              Object.keys(selectedRiskStockData).map(key => {
 
-        if (key === "riskScore" || key === "_id") return;
+                if (key === "riskScore" || key === "_id") return;
 
-        const title = stockKeyNameMappings.find(mapping => mapping.key === key)?.name
-        const value = selectedRiskStockData[key]
+                const value = selectedRiskStockData[key]
+                if (value === 0.0) return;
 
-        return (
-          <h4>{title}: {value}%</h4>
-        )
-      
-      
-      })}
+                const title = stockKeyNameMappings.find(mapping => mapping.key === key)?.name
+                const bgColor = stockKeyNameMappings.find(mapping => mapping.key === key)?.bgColor
 
+                return (
+                  <div className='text-white flex text-18px'>
+                    <p className='percentage-bar-label margin-0'>{title}</p>
+
+                    <div style={{ width: `${value * 1500}px`, backgroundColor: bgColor }} className='percentage-bar'>
+                      {(value * 100).toFixed(0)} %
+                    </div>
+                  </div>
+                )
+              })
+
+            )}
+          </div>
+
+          <div className='zero-stocks-section'>
+            {selectedRiskStockData !== undefined && (
+
+              Object.keys(selectedRiskStockData).map(key => {
+
+                if (key === "riskScore" || key === "_id") return;
+
+                const value = selectedRiskStockData[key]
+                if (value !== 0.0) return;
+
+                const title = stockKeyNameMappings.find(mapping => mapping.key === key)?.name
+
+                return (
+                  <div className='zero-stock-text'>
+                    <span>{title}</span>
+                    <span>{value}%</span>
+                  </div>
+                )
+              })
+
+            )}
+          </div>
+
+
+        </div>
+
+        <div className='right-section'>
+
+          <div className='right-section-info'>
+            <h4 className='text-40 margin-0'>Smarter investing, brilliantly personalized.</h4>
+            <p>Just answer a few questions, and we’ll build you a personalized portfolio of low-cost index funds from up to 17 global asset classes. Our software handles all the trading, rebalancing, and other busywork to help grow your wealth for the long term.</p>
+            <button className='get-started-btn'>Get Started</button>
+          </div>
+          
+        </div>
+
+      </div>
 
     </div>
   );
